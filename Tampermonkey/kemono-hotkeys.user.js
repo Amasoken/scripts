53a72a969b636b2f0e4c6656229137d1859c4c37
Amasoken <amasoken@gmail.com>
2026-06-30 02:52:39 +0300

    Update hotkeys

--- Tampermonkey/kemono-hotkeys.user.js
+++ Tampermonkey/kemono-hotkeys.user.js
@@ -44,71 +44,123 @@
     const MIN_Y = -10;
     const MAX_Y = 10;
 
-    const actions = {
-        // Jump to the first image
-        4: () => {
-            const images = getImages();
-            images[0]?.image?.scrollIntoView();
-        },
-        // Jump to the last image
-        1: () => {
-            const images = getImages();
-            images.at(-1)?.image?.scrollIntoView();
-        },
-        // Jump to the next image
-        5: () => {
-            const images = getImages();
-            const index = images.findIndex((e) => e.rect.y > MIN_Y);
-            const img = images[index - 1] ?? images[index] ?? images.at(0);
-            img?.image?.scrollIntoView();
-        },
-        // Jump to the previous image
-        2: () => {
-            const images = getImages();
-            const index = images.findIndex((e) => e.rect.y > MAX_Y);
-            const img = images[index] ?? images.at(-1);
-            img?.image?.scrollIntoView();
-        },
-        // Favorite
-        f: () => {
-            document.querySelector('button[class*="favoriteButton"], button.post__fav')?.click();
-        },
-        // Favorite
-        '.': () => {
-            document.querySelector('button[class*="favoriteButton"], button.post__fav')?.click();
-        },
-        // emulate alt+RMB, which is used for saving images in https://github.com/Amasoken/scripts/raw/master/Tampermonkey/imageSaver.user.js
-        7: () => {
-            const images = getImages();
-            const index = images.findIndex((e) => e.rect.y > MAX_Y);
-
-            const img = images[index - 1] ?? images[index] ?? images.at(-1);
-            simulateClick(img?.image, 'contextmenu', {
-                bubbles: true,
-                cancelable: true,
-                view: window,
-                altKey: true,
-                button: 2,
-            });
-        },
-        // Expand current image (clicks on preview)
-        8: () => {
-            const images = getImages();
-            const index = images.findIndex((e) => e.rect.y > MAX_Y);
-            const img = images[index - 1] ?? images[index] ?? images.at(-1);
-            img?.image?.click();
-        },
-        // Expand all images in a post
-        9: () => {
-            const images = getImages();
-            for (const image of images) {
-                image.image?.click();
-            }
-        },
-        0: () => {
-            window.history.back();
-        },
+    // ACTIONS
+    const jumpToTopmostImage = () => {
+        const images = getImages();
+        images[0]?.image?.scrollIntoView();
     };
+
+    const jumpToBottommostImage = () => {
+        const images = getImages();
+        images.at(-1)?.image?.scrollIntoView();
+    };
+
+    const jumpToUpperImage = () => {
+        const images = getImages();
+        const index = images.findIndex((e) => e.rect.y > MIN_Y);
+        const img = images[index - 1] ?? images[index] ?? images.at(0);
+        img?.image?.scrollIntoView();
+    };
+
+    const jumpToLowerImage = () => {
+        const images = getImages();
+        const index = images.findIndex((e) => e.rect.y > MAX_Y);
+        const img = images[index] ?? images.at(-1);
+        img?.image?.scrollIntoView();
+    };
+
+    const favorite = () => {
+        const selectors = [
+            'button[class*="favoriteButton"]:not([class*="unfav"])', // user page
+            'button.user-header__favourite:not([class*="unfav"])', // paw user page
+            'button.post__fav:not(.post__fav--unfav)', // post page
+        ];
+        document.querySelector(selectors.join(','))?.click();
+    };
+
+    const unFavorite = () => {
+        const selectors = [
+            'button[class*="favoriteButton"][class*="unfav"]', // user page
+            'button.user-header__favourite[class*="unfav"]', // paw user page
+            'button.post__fav.post__fav--unfav', // post page
+        ];
+        document.querySelector(selectors.join(','))?.click();
+    };
+
+    // emulate alt+RMB, which is used for saving images in https://github.com/Amasoken/scripts/raw/master/Tampermonkey/imageSaver.user.js
+    const altRmbClickOnImage = () => {
+        const images = getImages();
+        const index = images.findIndex((e) => e.rect.y > MAX_Y);
+
+        const img = images[index - 1] ?? images[index] ?? images.at(-1);
+        simulateClick(img?.image, 'contextmenu', {
+            bubbles: true,
+            cancelable: true,
+            view: window,
+            altKey: true,
+            button: 2,
+        });
+    };
+
+    const expandCurrentImage = () => {
+        const images = getImages();
+        const index = images.findIndex((e) => e.rect.y > MAX_Y);
+        const img = images[index - 1] ?? images[index] ?? images.at(-1);
+        img?.image?.click();
+    };
+
+    const expandAllImages = () => {
+        const images = getImages();
+        for (const image of images) {
+            image.image?.click();
+        }
+    };
+
+    const goBack = () => {
+        window.history.back();
+    };
+
+    const goForward = () => {
+        window.history.forward();
+    };
+
+    // ========================================
+    const hotkeys = {};
+
+    const addHotkey = (keys, action) => {
+        for (const key of keys) {
+            hotkeys[key] = action;
+        }
+    };
+
+    // Known limitations:
+    // do not use shift + numpad keys - shift temporarily switches numpad state, and modifier key is not held
+    // shift + NumpadDecimal == ShiftLeft -> NumpadDecimal (no modifiers) -> ShiftLeft
+    // Too much work to work around it, so just avoid shift on numpad keys
+    addHotkey(['Numpad4', 'Ctrl+Numpad5'], jumpToTopmostImage);
+    addHotkey(['Numpad1', 'Ctrl+Numpad2'], jumpToBottommostImage);
+    addHotkey(['Numpad2'], jumpToLowerImage);
+    addHotkey(['Numpad5'], jumpToUpperImage);
+
+    addHotkey(['KeyF', 'NumpadDecimal'], favorite);
+    addHotkey(['Shift+KeyF', 'Ctrl+NumpadDecimal'], unFavorite);
+
+    addHotkey(['Numpad7'], altRmbClickOnImage);
+    addHotkey(['Numpad8'], expandCurrentImage);
+    addHotkey(['Numpad9'], expandAllImages);
+
+    addHotkey(['Numpad0'], goBack);
+    addHotkey(['Ctrl+Numpad0'], goForward);
+
+    function getHotkeyString(e) {
+        const parts = [];
+        if (e.ctrlKey) parts.push('Ctrl');
+        if (e.shiftKey) parts.push('Shift');
+        if (e.altKey) parts.push('Alt');
+        parts.push(e.code);
+
+        return parts.join('+');
+    }
 
     function simulateClick(el, type = 'click', options = {}) {
         const event = new MouseEvent(type, options);
@@ -116,7 +168,16 @@
     }
 
     document.addEventListener('keydown', (e) => {
-        // console.log(e.key);
-        actions[e.key] && actions[e.key]();
+        const target = e.target;
+        if (target instanceof HTMLInputElement || target instanceof HTMLTextAreaElement || target.isContentEditable) {
+            return;
+        }
+
+        const hotkeyString = getHotkeyString(e);
+
+        if (hotkeys[hotkeyString]) {
+            e.preventDefault();
+            hotkeys[hotkeyString]();
+        }
     });
 })();
